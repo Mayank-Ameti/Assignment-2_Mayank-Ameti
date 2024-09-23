7a35e2a2ac4489b3fbbffdeac5758d57211dc0d5
Mayank-Ameti <Mameti0099@conestogac.on.ca>
2024-09-23 19:09:57 -0400

    Initial Source Code updated

--- Assignment 2/Source.cpp
+++ Assignment 2/Source.cpp
@@ -1,3 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include<sstream>
+#include <string>
+
+using namespace std;
+
+struct STUDENTDATA {
+	string FirstName;
+	string LastName;
+};
+
 int main() {
+
+	ifstream Inputfile("StudentData.txt");
+	if (!Inputfile.is_open()) {
+
+		cerr << "Unable to open file!" << std::endl;
+		return 1;
+	}
+
+	vector <STUDENTDATA> students;
+
+	string line;
+
+	// Reading each line from a file
+	while (getline(Inputfile, line)) {
+		istringstream parse(line);
+		string FirstName, LastName;
+
+		getline(parse, LastName, ',');
+		getline(parse, FirstName);
+
+		//Creating a Student Object
+		STUDENTDATA student{ FirstName, LastName };
+		//Storing into vector space
+		students.push_back(student);
+	}
+	Inputfile.close();
+
+	//Prints the Parsed data
+	/*for (const auto& student : students) {
+		cout << "Last Name: " << student.LastName << ", First Name: " << student.FirstName << std::endl;
+	}*/
+
 	return 1;
 }
