8f7965cd1d5d908a5b15b602135786f28d49ffab
Mayank-Ameti <Mameti0099@conestogac.on.ca>
2024-09-23 21:37:21 -0400

    Updated PreRelease Branch

--- Assignment 2/Source.cpp
+++ Assignment 2/Source.cpp
@@ -9,11 +9,20 @@ using namespace std;
 struct STUDENTDATA {
 	string FirstName;
 	string LastName;
+	string StudentEmail;
 };
 
 int main() {
 
+#ifdef PRE_RELEASE
+	cout << "PreRelease Version Source Code Is Running Now" << endl;
+	ifstream Inputfile("StudentData_Emails.txt");
+#else
+	cout << "Standard Version Source Code Is Running Now" << endl;
 	ifstream Inputfile("StudentData.txt");
+#endif
+
+	
 	if (!Inputfile.is_open()) {
 
 		cerr << "Unable to open file!" << std::endl;
@@ -27,13 +36,16 @@ int main() {
 	// Reading each line from a file
 	while (getline(Inputfile, line)) {
 		istringstream parse(line);
-		string FirstName, LastName;
+		string FirstName, LastName,StudentEmail;
 
 		getline(parse, LastName, ',');
-		getline(parse, FirstName);
+		getline(parse, FirstName,',');
 
+		#ifdef PRE_RELEASE
+		getline(parse, StudentEmail);  // Read email if in pre-release mode
+		#endif
 		//Creating a Student Object
-		STUDENTDATA student{ FirstName, LastName };
+		STUDENTDATA student{ FirstName, LastName , StudentEmail };
 		//Storing into vector space
 		students.push_back(student);
 	}
@@ -42,7 +54,13 @@ int main() {
 	//Prints the Parsed data
 #ifdef DEBUG
 	for (const auto& student : students) {
-		cout << "Last Name: " << student.LastName << ", First Name: " << student.FirstName << std::endl;
+		cout << "Last Name: " << student.LastName << ", First Name: " << student.FirstName << endl;
+	}
+#endif
+
+#ifdef PRE_RELEASE
+	for (const auto& student : students) {
+		cout << "Last Name: " << student.LastName << ", First Name: " << student.FirstName << " , Email: " << student.StudentEmail << endl;
 	}
 #endif
 	Inputfile.close();
